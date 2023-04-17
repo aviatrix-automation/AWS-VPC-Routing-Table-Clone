d33fb10d7e0d58c682aa5c48a8e3efb5d638b255
Sujit Nair <107117417+sujitn24@users.noreply.github.com>
2023-04-17 16:17:40 +0530

    Update routebotov4.py

--- routebotov4.py
+++ routebotov4.py
@@ -25,65 +25,62 @@ else:
     vpc_ids = args.vpc_ids
     
 for vpc_id in vpc_ids:
-    # Retrieve the route tables for the current VPC
-    route_tables = ec2.describe_route_tables(Filters=[{'Name': 'vpc-id', 'Values': [vpc_id]}])['RouteTables']
+    # Retrieve the main route table for the current VPC
+    route_tables = ec2.describe_route_tables(Filters=[
+        {'Name': 'vpc-id', 'Values': [vpc_id]},
+        {'Name': 'association.main', 'Values': ['true']}
+    ])['RouteTables']
     
+    if not route_tables:
+        print(f"No main route table found for VPC: {vpc_id}")
+        continue
+
+    main_route_table = route_tables[0]
+    main_route_table_id = main_route_table['RouteTableId']
+    
+    print(f"Main Route table is {main_route_table_id} in VPC: {vpc_id}")
+
     # Create a new route table
     new_route_table = ec2.create_route_table(VpcId=vpc_id)
     new_route_table_id = new_route_table['RouteTable']['RouteTableId']
+    
 
-    main_route_table_id = ''
-    for route_table in route_tables:
-        for association in route_table['Associations']:
-            if association['Main']:
-                main_route_table_id = route_table['RouteTableId']
-                break
-        if main_route_table_id:
-            break
-
-    print(f"Main Route table is {main_route_table_id} in VPC: {vpc_id}")
-    # Iterate over the list of route tables and duplicate them
-    for route_table in route_tables:
-        # Check if the route table is the main route table of the VPC
-        if route_table['Associations'] and 'Main' in route_table['Associations'][0]:
-            continue  # Skip the main route table
-
-        # Duplicate the routes from the existing route table to the new route table
-        for route in route_table['Routes']:
-            if 'GatewayId' in route and not route['GatewayId'].startswith('local'):
-                ec2.create_route(
-                    DestinationCidrBlock=route['DestinationCidrBlock'],
-                    GatewayId=route['GatewayId'],
-                    RouteTableId=new_route_table_id
-                )
-            elif 'TransitGatewayId' in route:
-                ec2.create_route(
-                    DestinationCidrBlock=route['DestinationCidrBlock'],
-                    TransitGatewayId='tgw-08bd8dcab1f3487eb',
-                    RouteTableId=new_route_table_id
-                )
-            elif 'NatGatewayId' in route:
-                ec2.create_route(
-                    DestinationCidrBlock=route['DestinationCidrBlock'],
-                    NatGatewayId=route['NatGatewayId'],
-                    RouteTableId=new_route_table_id
-                )
-            elif 'VpcPeeringConnectionId' in route:
-                ec2.create_route(
-                    DestinationCidrBlock=route['DestinationCidrBlock'],
-                    VpcPeeringConnectionId=route['VpcPeeringConnectionId'],
-                    RouteTableId=new_route_table_id
-                )
-            elif 'InstanceId' in route:
-                ec2.create_route(
-                    DestinationCidrBlock=route['DestinationCidrBlock'],
-                    VpcPeeringConnectionId=route['InstanceId'],
-                    RouteTableId=new_route_table_id
-                )
-            elif 'NetworkInterfaceId' in route:
-                ec2.create_route(
-                    DestinationCidrBlock=route['DestinationCidrBlock'],
-                    VpcPeeringConnectionId=route['NetworkInterfaceId'],
-                    RouteTableId=new_route_table_id
-                )
+    # Duplicate the routes from the main route table to the new route table
+    for route in main_route_table['Routes']:
+        if 'GatewayId' in route and not route['GatewayId'].startswith('local'):
+            ec2.create_route(
+                DestinationCidrBlock=route['DestinationCidrBlock'],
+                GatewayId=route['GatewayId'],
+                RouteTableId=new_route_table_id
+            )
+        elif 'TransitGatewayId' in route:
+            ec2.create_route(
+                DestinationCidrBlock=route['DestinationCidrBlock'],
+                TransitGatewayId=route['TransitGatewayId'],
+                RouteTableId=new_route_table_id
+            )
+        elif 'NatGatewayId' in route:
+            ec2.create_route(
+                DestinationCidrBlock=route['DestinationCidrBlock'],
+                NatGatewayId=route['NatGatewayId'],
+                RouteTableId=new_route_table_id
+            )
+        elif 'VpcPeeringConnectionId' in route:
+            ec2.create_route(
+                DestinationCidrBlock=route['DestinationCidrBlock'],
+                VpcPeeringConnectionId=route['VpcPeeringConnectionId'],
+                RouteTableId=new_route_table_id
+            )
+        elif 'InstanceId' in route:
+            ec2.create_route(
+                DestinationCidrBlock=route['DestinationCidrBlock'],
+                VpcPeeringConnectionId=route['InstanceId'],
+                RouteTableId=new_route_table_id
+            )
+        elif 'NetworkInterfaceId' in route:
+            ec2.create_route(
+                DestinationCidrBlock=route['DestinationCidrBlock'],
+                VpcPeeringConnectionId=route['NetworkInterfaceId'],
+                RouteTableId=new_route_table_id
+            )
     print(f"New route table created with ID: {new_route_table_id} in VPC: {vpc_id}")
